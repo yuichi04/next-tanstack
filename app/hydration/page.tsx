df007a1650c047449347c152f34e66eaa136265b
yuichi04 <crgone.910428.en@gmail.com>
2024-09-13 07:25:07 +0900

    first commit

---
+++ app/hydration/page.tsx
@@ -0,0 +1,19 @@
+import { dehydrate } from "@tanstack/query-core";
+import ListUsers from "./list-users";
+import { HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { getUsers } from "@/utils/api-requests";
+
+export default async function Page() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["hydrate-users"],
+    queryFn: getUsers,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ListUsers />
+    </HydrationBoundary>
+  );
+}
